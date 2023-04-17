05a0e9caca9cd8bb0b1fc6214a2dafa0d1d4c5fe
Nina <nina.emiko.mcnair@gmail.com>
2023-04-17 10:46:38 -0400

    Added method to convert JerkSON data to an array

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,48 @@
 import org.apache.commons.io.IOUtils;
-import java.io.IOException;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
     public String readRawDataToString() throws Exception{
         ClassLoader classLoader = getClass().getClassLoader();
-        String result = IOUtils.toString(classLoader.getResourceAsStream("RawData.txt"));
-        return result;
+        return IOUtils.toString(classLoader.getResourceAsStream("RawData.txt"));
     }
 
     public static void main(String[] args) throws Exception{
         String output = (new Main()).readRawDataToString();
-        System.out.println(output);
-
+        String[] keyValueArray = convertJerkSONToArray(output);
+        printArray(keyValueArray);
     }
+
+    public static void printArray(String[] arr) {
+        for (String s: arr) {
+            System.out.println(s);
+        }
+    }
+
+    public static String[] convertJerkSONToArray(String s){
+        Pattern pattern = Pattern.compile("\\w+:[^#]+");
+        Matcher matcher = pattern.matcher(s);
+
+        String[] individualJawns = new String[0];
+
+        while(matcher.find()) {
+            individualJawns = Arrays.copyOf(individualJawns, individualJawns.length + 1);
+            individualJawns[individualJawns.length - 1] = matcher.group();
+        }
+        return individualJawns;
+    }
+
+    //toLowerCase
+    //if thisString.get(4) starts with c
+    //if starts with a
+    //if starts with m
+    //if starts with b
+    //if "" increment error's seen value
 }
+
+//naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##
